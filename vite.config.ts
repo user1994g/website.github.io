04d2889c3d21f5092b9e7e19fe4aa6c463bc731d
user1994g <jazzg869@gmail.com>
2026-03-06 03:26:39 +0000

    Fix deployment environment and base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/website.github.io/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
